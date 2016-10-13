94b0fec3f0ff4b9e512d268bce75fffae1fba91b
Pablo Castellano <pablo@anche.no>
2016-10-13 17:55:00 +0200

    Migrated from South

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ import sys
 
 if __name__ == "__main__":
     os.environ.setdefault('DJANGO_SETTINGS_MODULE',
-                          'ad_rotator.tests.south_settings')
+                          'ad_rotator.tests.test_settings')
 
     from django.core.management import execute_from_command_line
 
